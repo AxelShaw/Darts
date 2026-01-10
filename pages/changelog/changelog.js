38fbc9f163cbe68acdc96e96d1e45e9554138024
AxelShaw <114676277+AxelShaw@users.noreply.github.com>
2026-01-10 15:55:55 +0100

    test

--- pages/changelog/changelog.js
+++ pages/changelog/changelog.js
@@ -8,48 +8,40 @@ class Changelog extends Page {
     }
     
     static async onOpen() {
+        const data = await api('releases');
+        
         let html = '<div class="changelog-container">';
         html += '<div class="page-header">';
         html += '<h1>Changelog</h1>';
-        html += '<p class="version">Version actuelle: <span id="current-version">...</span></p>';
+        html += '<p class="version">Version actuelle: <span>' + (data.current_version || '...') + '</span></p>';
         html += '</div>';
-        html += '<div id="releases">Chargement...</div>';
+        
+        if (data.success && data.releases.length > 0) {
+            data.releases.forEach(release => {
+                const date = new Date(release.date).toLocaleDateString('fr-FR', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric'
+                });
+                
+                const displayTag = release.tag.replace(/^v/, '');
+                
+                html += '<div class="release">';
+                html += '<div class="release-header">';
+                html += '<span class="release-tag">' + displayTag + '</span>';
+                html += '<span class="release-date">' + date + '</span>';
+                html += '</div>';
+                html += '<h3 class="release-name">' + (release.name || release.tag) + '</h3>';
+                html += '<div class="release-description">' + Changelog.formatDescription(release.description) + '</div>';
+                html += '</div>';
+            });
+        } else if (data.error) {
+            html += '<p class="error">Erreur: ' + data.error + '</p>';
+        }
+        
         html += '</div>';
         
         Changelog.setContent(html);
-        
-        const data = await api('releases');
-        
-        if (data.success) {
-            document.getElementById('current-version').textContent = data.current_version;
-            
-            let releasesHtml = '';
-            
-            if (data.releases.length > 0) {
-                data.releases.forEach(release => {
-                    const date = new Date(release.date).toLocaleDateString('fr-FR', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: 'numeric'
-                    });
-                    
-                    const displayTag = release.tag.replace(/^v/, '');
-                    
-                    releasesHtml += '<div class="release">';
-                    releasesHtml += '<div class="release-header">';
-                    releasesHtml += '<span class="release-tag">' + displayTag + '</span>';
-                    releasesHtml += '<span class="release-date">' + date + '</span>';
-                    releasesHtml += '</div>';
-                    releasesHtml += '<h3 class="release-name">' + (release.name || release.tag) + '</h3>';
-                    releasesHtml += '<div class="release-description">' + Changelog.formatDescription(release.description) + '</div>';
-                    releasesHtml += '</div>';
-                });
-            }
-            
-            document.getElementById('releases').innerHTML = releasesHtml;
-        } else {
-            document.getElementById('releases').innerHTML = '<p class="error">Erreur: ' + data.error + '</p>';
-        }
     }
     
     static formatDescription(text) {
